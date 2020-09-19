c51f3787e0bf26750e28f8a077a393d6df47dbd8
bautiblacker <bblacker@itba.edu.ar>
2020-09-19 20:58:49 -0300

    fixes

--- SimplePerceptron.py
+++ SimplePerceptron.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import random
+import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,51 +14,59 @@ class SimplePerceptron:
         self.entries = entries
         self.steps = steps
         self.output = output
+        self.weightsInitializer()
 
     def displaySteps(self):
         print(self.steps)
 
     def weightsInitializer(self):
-        for idx in range(self.steps):
+        for idx in range(len(self.entries)):
             SimplePerceptron.weights.append(round(random.random(), 5))
 
     def get_excitement(self, entries):
         total = 0
         for e,w in zip(entries, self.weights):
-            total += e*w
+            total += np.sum(np.dot(e,w))
 
         return total
 
-    def update_wights(self, delta_wights):
-        for w, dw in zip(self.weights, delta_wights):
-            w += dw
+    def update_wights(self, delta_weights):
+        for i in range(len(self.weights)):
+            self.weights[i] += delta_weights[i]
 
     def perform(self):
         i = 0
-        error = 1
+        size = len(entries)
+        error = 2 * size
+
         plt.ylabel('error')
         plt.xlabel('activacion')
+        x_errors = []
+        y_activation = []
+        z_excitements = []
+
         while error > 0 and i < self.steps:
-            print('while')
-            for idx in range(len(self.output)):
-                excitement = self.get_excitement(self.entries[i])
+            for idx in range(size):
+                excitement = self.get_excitement(self.entries[idx])
                 activation = np.sign(excitement)
-                error = self.output[idx] - activation
+                error -= self.output[idx] - activation
                 delta_weights = np.dot(self.learning_grade * error, self.entries[idx])
                 self.update_wights(delta_weights)
-                plt.plot(error, activation)
-                print(error)
+                x_errors.append(error)
+                y_activation.append(activation)
+                print(excitement)
+            print('\n')
             i = i + 1
+        # plt.plot(y_activation,x_errors)
+        # plt.show()
 
-        plt.show()
         return
 
 
 
 entries = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
 output = [1, 1, -1, -1]
-sp1 = SimplePerceptron(0.5, entries, output, 2)
-sp1.weightsInitializer()
+sp1 = SimplePerceptron(0.5, entries, output, 10)
 sp1.perform()
 
 # x = [1,2,3,4,5,6,7]
@@ -67,3 +76,11 @@ sp1.perform()
 # plt.ylabel('y')
 # plt.xlabel('x')
 # plt.show()
+
+# wight initializer
+print('wieght initializer')
+print(sp1.weights)
+
+# wight initializer
+print('get excitement')
+print(sp1.get_excitement(entries))
