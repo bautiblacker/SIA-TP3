54f7fe7521fd7c3b142fa7153ffdf065df867d8c
bautiblacker <bblacker@itba.edu.ar>
2020-09-30 20:34:56 -0300

    graphs

--- ParityResolver.py
+++ ParityResolver.py
@@ -18,14 +18,15 @@ class ParityResolver:
         training_qty = configuration['multilayer_test_qty']
         max_training_epochs = configuration['multilayer_max_training_epochs']
 
-        mp = MultilayerPerceptron(input_nodes_qty, hidden_nodes_qty, output_nodes_qty, lr)
-
         pb_entries = fp.mlp_entries_parser(7,5)
         pb_targets = [('0'), ('1'), ('0'), ('1'), ('0'), ('1'), ('0'), ('1'), ('0'), ('1')]
+        mp = MultilayerPerceptron(pb_entries, pb_targets, input_nodes_qty, hidden_nodes_qty, output_nodes_qty, lr)
 
-        for i in range(max_training_epochs):
-            x = random.randint(0,training_qty)
-            mp.train(np.matrix(pb_entries[x]).transpose(),np.matrix(pb_targets[x]).transpose())
+        # for i in range(max_training_epochs):
+        #     x = random.randint(0,training_qty)
+        #     mp.train(np.matrix(pb_entries[x]).transpose(),np.matrix(pb_targets[x]).transpose())
+
+        mp.train()
 
         plot_entries = []
         for pb_e in pb_entries:
--- XorResolver.py
+++ XorResolver.py
@@ -17,15 +17,15 @@ class XorResolver:
         output_nodes_qty = 1
         lr = configuration['multilayer_lr']
 
-        mp = MultilayerPerceptron(input_nodes_qty,hidden_nodes_qty,output_nodes_qty,lr)
-
         pa_entries = [('1 0'),('0 1'), ('0 0'), ('1 1')]
         pa_targets = [('1'), ('1'), ('0'), ('0')]
+        mp = MultilayerPerceptron(pa_entries, pa_targets, input_nodes_qty,hidden_nodes_qty,output_nodes_qty,lr)
 
-        for i in range(10000):
-            x = random.randint(0,3)
-            mp.train(np.matrix(pa_entries[x]).transpose(),np.matrix(pa_targets[x]).transpose())
+        # for i in range(10000):
+        #     x = random.randint(0,3)
+        #     mp.train(np.matrix(pa_entries[x]).transpose(),np.matrix(pa_targets[x]).transpose())
 
+        mp.train()
         print(mp.feed_forward(np.matrix(('1 0')).transpose()))
         print(mp.feed_forward(np.matrix(('0 1')).transpose()))
         print(mp.feed_forward(np.matrix(('0 0')).transpose()))
